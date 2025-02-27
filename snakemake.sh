5408dd2043996d8e22b657949a12a712c659ada4
Adam Catching <adamcatching@gmail.com>
2025-02-27 11:32:44 -0500

    Snakefile script can now be changed to switch between workflows

--- snakemake.sh
+++ snakemake.sh
@@ -36,8 +36,11 @@ module load singularity/4.1.5
 # Bind external directories on Biowulf
 . /usr/local/current/singularity/app_conf/sing_binds
 
+# In the future the snakefile will be changed depending on workflow
+snakefile_name="snakefile"
+
 # RUN SCRIPT
-snakemake --cores all --profile snakemake_profile --use-singularity
+snakemake --cores all --profile snakemake_profile --use-singularity --use_snakefile $snakefile_name
 
 #! WARNING - if the slurm-*.txt files say that there is a locked file error, then:
 # - uncomment the --unlock flag above
