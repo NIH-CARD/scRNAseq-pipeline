abd862db8589f9651c419975e48fb30843088023
Adam Catching <adamcatching@gmail.com>
2025-03-18 11:28:18 -0400

    Scrublet is called after QC filtering

--- scripts/rna_filter.py
+++ scripts/rna_filter.py
@@ -9,13 +9,17 @@ adata = sc.read_h5ad(snakemake.input.rna_anndata) # type: ignore
 
 # Threshold below a given mitochondria percent
 adata = adata[adata.obs['pct_counts_mt'] < snakemake.params.mito_percent_thresh].copy()
-# Threshold below a given doublet score
-adata = adata[adata.obs['doublet_score'] < snakemake.params.doublet_thresh].copy()
 # Threshold above a given genes per cell threshold
 adata = adata[adata.obs['n_genes_by_counts'] > snakemake.params.min_genes_per_cell].copy()
 # Threshold below a given ribosome threshold
 adata = adata[adata.obs['pct_counts_rb'] < snakemake.params.ribo_percent_thresh].copy()
 
+# Recompute doublet score based on filtered data
+sc.pp.scrublet(adata, expected_doublet_rate=(adata.n_obs / 1000) * 0.008, threshold=0.15, n_prin_comps=10)
+# Threshold below a given doublet score
+adata = adata[adata.obs['doublet_score'] < snakemake.params.doublet_thresh].copy()
+
+# Recompute log-normalized data from filtered dataset
 if adata.n_obs != 0:
     # Normalize data
     sc.pp.normalize_total(adata)
@@ -27,7 +31,7 @@ if adata.n_obs != 0:
     sc.pp.log1p(adata, layer='cpm')
 
     # Save the normalized-log data
-    adata.layers['data']=adata.X.copy() 
+    adata.layers['log-norm']=adata.X.copy() 
 
 # Write out filtered anndata object
 adata.write(filename=snakemake.output.rna_anndata, compression='gzip') # type: ignore
