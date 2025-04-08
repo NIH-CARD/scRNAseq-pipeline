76796480264a37f7f85e765ffff55fcc4737e30c
Adam Catching <adamcatching@gmail.com>
2025-04-08 13:44:38 -0400

    Minor snakefile error

--- scripts/merge_muon.py
+++ scripts/merge_muon.py
@@ -3,4 +3,4 @@ import scanpy as sc
 
 mdata = mu.MuData({"RNA": sc.read_h5ad(snakemake.input.merged_rna_anndata), "ATAC": sc.read_h5ad(snakemake.input.merged_atac_anndata)})
 
-mdata.write_h5mu(snakefile.output.merged_multiome)
+mdata.write_h5mu(snakemake.output.merged_multiome)
