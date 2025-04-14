1ab92fe9a87c2b78b6ad4eeab345aa090a7becd3
Adam Catching <adamcatching@gmail.com>
2025-04-14 12:03:39 -0400

    Remove singularity version dependence

--- scripts/rna_model.sh
+++ scripts/rna_model.sh
@@ -8,6 +8,6 @@ merged_rna_anndata=$4
 model=$5
 
 # Load module
-module load singularity/4.1.5
+module load singularity
 # Run 
 singularity run --nv --bind "$PWD" envs/single_cell_gpu.sif python scripts/rna_model.py "${input_file}" "${sample_key}" "${model_history}" "${merged_rna_anndata}" "${model}"
